a2ebdb4838c00869458609f74b925f3efbe0070a
Šimon Trousil <simon.trousil@seznam.cz>
2023-01-24 16:22:24 +0000

    login added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Providers, LoginType} from '@microsoft/mgt-element';
+import {Msal2Provider, PromptType} from '@microsoft/mgt-msal2-provider';
+
+Providers.globalProvider = new Msal2Provider({ clientId: 'c25f4378-030c-4502-897b-45f3ba069317', authority: "https://login.microsoftonline.com/2b51a4b3-443f-4406-8ca4-19056a79a444",
+redirectUri: "https://3000-simik394-onenoteorganiz-j5knvh6w7bi.ws-eu83.gitpod.io"})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
