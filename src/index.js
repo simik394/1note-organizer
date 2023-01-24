97b8eb6e1774a3bcba2e0c07f225e3a5fc001556
Šimon Trousil <simon.trousil@seznam.cz>
2023-01-24 16:58:25 +0000

    +

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Providers, LoginType} from '@microsoft/mgt-element';
 import {Msal2Provider} from '@microsoft/mgt-msal2-provider';
 
 Providers.globalProvider = new Msal2Provider({ clientId: 'c25f4378-030c-4502-897b-45f3ba069317', authority: "https://login.microsoftonline.com/2b51a4b3-443f-4406-8ca4-19056a79a444",
-redirectUri: "https://3000-simik394-onenoteorganiz-j5knvh6w7bi.ws-eu83.gitpod.io", LoginType: "popup"})
+redirectUri: "https://simik394.github.io/oneNote-organizer/", LoginType: "popup"})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
