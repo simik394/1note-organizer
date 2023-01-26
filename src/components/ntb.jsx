391c6a28d178562b01b688732c280f8dd3275129
Šimon Trousil <simon.trousil@seznam.cz>
2023-01-26 22:23:13 +0000

    try

--- src/components/ntb.jsx
+++ src/components/ntb.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Providers, ProviderState } from '@microsoft/mgt'
+import { Button } from '@blueprintjs/core';
 
 
 
 
 
 function Ntbks() {
-
      async function callG(){
          const provider = Providers.globalProvider;
          const gClient = provider.graph.client;
-         const JsonData = await gClient.api('/me/onenote/notebooks/').get(); //.then((res) => res.json()); //.then((res) => res.json()).then((data) => console.log(data) );
-         return (JsonData)
+         const JsonData = await gClient.api('/me/onenote/notebooks/').get()
+         .then((res) => res.value); //.then((res) => res.json()).then((data) => console.log(data) );
+         return (console.log(JsonData))
      }
      
     const ntb = [{
@@ -28,8 +30,10 @@ function Ntbks() {
     const DisplayData=ntb.map(
         (info)=>{return ( <tr>
             <td>{info.id}</td>
-            <td>{info.name}</td>
+            <td>{info.displayName}</td>
             <td>{info.city}</td>
+            <Button onClick={callG()}>callG</Button>
+            
         </tr>
     )});
 
