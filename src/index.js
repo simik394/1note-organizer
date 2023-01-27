453307f7089b37069bb4c124e072ead2418a7a38
Šimon Trousil <simon.trousil@seznam.cz>
2023-01-27 10:03:02 +0000

    index clened

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
 const logType = LoginType.Popup;
 Providers.globalProvider = new Msal2Provider({ clientId: 'c25f4378-030c-4502-897b-45f3ba069317', authority: "https://login.microsoftonline.com/2b51a4b3-443f-4406-8ca4-19056a79a444",
-redirectUri: "https://3000-simik394-onenoteorganiz-792rr05qxhi.ws-eu84.gitpod.io", loginType: logType})
+redirectUri: "https://simik394-1noteorganizer-6nbbxea0my5.ws-eu84.gitpod.io/", loginType: logType})
 const provider = Providers.globalProvider;
-export const gClient = provider.graph.client;
+const gClient = provider.graph.client;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App gCli={gClient} />
+    <App  />
   </React.StrictMode>
 );
 
