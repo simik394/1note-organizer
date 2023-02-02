65949bab5b0c1f7fa0dfb46ddf39d650ca3398f6
simik394 <simon.trousil@seznam.cz>
2023-02-02 01:32:41 +0100

    a

--- src/components/ntb.jsx
+++ src/components/ntb.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react'
-import { Providers } from '@microsoft/mgt'
 import { Button } from '@blueprintjs/core';
+
 import {CallG} from '../GS'
 //import { Notebook, PageCollection } from 'microsoft-graph';
 
 
 
 
-export default function Ntbks() {
+export default function Ntbks(props) {
 
     const [notebooks, setNotebooks] = useState();
     useEffect(() => {
@@ -23,30 +23,7 @@ export default function Ntbks() {
         //console.log("2 - "+notebooks);
     });
 
-    const ntb = [{
-        "id": 8,
-        "name": "nn",
-        "city": "111"
-    },
-
-    {
-        "id": 9,
-        "name": "jj",
-        "city": "222"
-    }];
-
-    const DisplayData = ntb.map(
-        (info) => {
-            return (
-                <>
-                    <li key={info.id}>{info.displayName}</li>
-
-                    
-                </>
-            )
-        });
-
-    const DD = <ul>{DisplayData}</ul>;
+    
 return (<Button onClick={CallG}>callG</Button>);
 
 
